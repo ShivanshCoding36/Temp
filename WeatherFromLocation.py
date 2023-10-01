0a318a0de6cdbfc87d3f84821504ef4e1b37b685
ShivanshCoding36 <101795319+ShivanshCoding36@users.noreply.github.com>
2023-10-01 12:43:11 +0530

    Update WeatherFromLocation.py

--- WeatherFromLocation.py
+++ WeatherFromLocation.py
@@ -4,7 +4,7 @@ def get_location():
     ip_address = requests.get("https://api.ipify.org").text
 #    print("IP obtained",ip_address)
 
-    api_key = "3e17fd00bdf342bfb86590d0c889e444"
+    api_key = "IP Geolocation api key"
     url = f"https://api.ipgeolocation.io/ipgeo?apiKey={api_key}&ip={ip_address}"
     response = requests.get(url).json()
     data = response
@@ -14,7 +14,7 @@ def get_location():
     return data
 
 def weather(loc):
-    api_key = "b55e90a4e47d360175b25a1a4b578363"
+    api_key = "open weather maps api key"
     city_name = loc
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}"
     response = requests.get(url)
